b3fda6ce786d9a39064ea18eacbfef8f0425400c
yohans14 <yohans14@yahoo.com>
2021-12-06 18:13:16 -0600

    add the input text and selceted value to the task list using innerHTML to add html tag in a string

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,18 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function(event) { 
   event.preventDefault(); 
 
-  var listItemEl = document.createElement("li"); 
-  listItemEl.className = "task-item"; 
-  listItemEl.textContent = "This is a new task."; 
-  tasksToDoEl.appendChild(listItemEl); 
-  }; 
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+var listItemEl = document.createElement("li");
+listItemEl.className = "task-item";
+
+var taskInfoEl = document.createElement("div");
+taskInfoEl.className = "task-info";
+taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+listItemEl.appendChild(taskInfoEl);
+
+tasksToDoEl.appendChild(listItemEl);
+}
   formEl.addEventListener("submit", createTaskHandler);
